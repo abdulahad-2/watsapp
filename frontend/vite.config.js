3a52f0cfe5271f2f97a84f8527ea25d5d7cf52ad
Abdul Ahad <ahadking6152@gmail.com>
2025-08-29 08:27:05 -0700

    solving svg nightmare

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // Ensure JSX in .js and .ts files is transformed
+      include: '**/*.{jsx,tsx,js,ts}',
+      jsxRuntime: 'automatic',
+    }),
+  ],
   server: {
     port: 3000,
   },
