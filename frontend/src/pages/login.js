283657db57d42c95147a101e1417bc76959e9994
Abdul Ahad <ahadking6152@gmail.com>
2025-08-29 07:32:53 -0700

    solving vercel error

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,13 +1,16 @@
+import React from "react";
 import LoginForm from "../components/auth/LoginForm";
 
 export default function Login() {
   return (
-    <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
-      {/*Container*/}
-      <div className="flex w-[1600px] mx-auto h-full">
-        {/*Login Form */}
-        <LoginForm />
-      </div>
-    </div>
+    React.createElement(
+      "div",
+      { className: "h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden" },
+      React.createElement(
+        "div",
+        { className: "flex w-[1600px] mx-auto h-full" },
+        React.createElement(LoginForm)
+      )
+    )
   );
 }
